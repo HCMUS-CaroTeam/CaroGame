560e53c98f4c74144248a93bffea8590860a233d
Lowen-here <tranthiloi.050406@gmail.com>
2026-03-17 23:05:31 +0700

    space để về menu

--- CaroGame/src/View/Menu.cpp
+++ CaroGame/src/View/Menu.cpp
@@ -19,11 +19,11 @@ void GotoXY(int x, int y)
 
 int AskContinue(int pWhoWin) {
 	if(pWhoWin == -1)
-		DrawText("Nguoi choi X thang, ban muon choi tiep? (ENTER/ESC)", 150, 550, 20, RED);
+		DrawText("Nguoi choi X thang, ban muon choi tiep? (ENTER/SPACE)", 150, 550, 20, RED);
 	else if (pWhoWin == 1)
-		DrawText("Nguoi choi O thang, ban muon choi tiep? (ENTER/ESC)", 150, 550, 20, RED);
+		DrawText("Nguoi choi O thang, ban muon choi tiep? (ENTER/SPACE)", 150, 550, 20, RED);
 	else if (pWhoWin == 0)
-		DrawText("Hai nguoi choi hoa, ban muon choi tiep? (ENTER/ESC)", 150, 550, 20, RED);
+		DrawText("Hai nguoi choi hoa, ban muon choi tiep? (ENTER/SPACE)", 150, 550, 20, RED);
 
 	if (IsKeyPressed(KEY_ENTER)) return 1;
 	if (IsKeyPressed(KEY_SPACE)) return -1;
