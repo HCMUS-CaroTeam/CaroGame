b6d3aed77d39ea63e8a0508b90bf1d40842bbd6e
pc <pc@DESKTOP-LPRBL9F>
2026-04-08 16:57:14 +0700

    1

--- CaroGame/src/Control/menu_data.cpp
+++ CaroGame/src/Control/menu_data.cpp
@@ -1,20 +1,32 @@
 #include "Control/menu_data.h"
+#include "Model/config.h"
 
-// Kích thước mỗi nút: 280 x 80 px
-// Màn hình: 1600 x 900
-// Bố cục 2 cột x 2 hàng, căn giữa màn hình:
-//   Tổng chiều ngang 2 nút + khoảng cách: 280 + 40 + 280 = 600
-//   Cột trái  x = 1600/2 - 300 = 500
-//   Cột phải  x = 1600/2 +  20 = 820
-//   Hàng 1    y = 460
-//   Hàng 2    y = 460 + 80 + 30 = 570
-
-NeonButton gButtons[] =
+Button gMainMenuButtons[] =
 {
-    { Vector2{ 500.0f, 460.0f }, "PLAY",    BTN_PLAY    },
-    { Vector2{ 820.0f, 460.0f }, "ABOUT",   BTN_ABOUT   },
-    { Vector2{ 500.0f, 570.0f }, "SETTING", BTN_SETTING },
-    { Vector2{ 820.0f, 570.0f }, "EXIT",    BTN_EXIT    }
+    { Vector2{ 205.0f, 690.0f }, "PLAY",    BTN_PLAY    },
+    { Vector2{ 510.0f, 690.0f }, "ABOUT",   BTN_ABOUT   },
+    { Vector2{ 815.0f, 690.0f }, "SETTING", BTN_SETTING },
+    { Vector2{ 1120.0f, 690.0f }, "EXIT",   BTN_EXIT    }
 };
 
-int gButtonCount = sizeof(gButtons) / sizeof(gButtons[0]);
+int gMainMenuButtonCount = sizeof(gMainMenuButtons) / sizeof(gMainMenuButtons[0]);
+
+Button gSetupButtons[] =
+{
+    { Vector2{ SCREEN_WIDTH * 0.5f - 140.0f, 690.0f }, "PLAY", SETUP_BTN_PLAY },
+    { Vector2{ SCREEN_WIDTH * 0.5f - 140.0f, 770.0f }, "BACK", SETUP_BTN_BACK }
+};
+
+int gSetupButtonCount = sizeof(gSetupButtons) / sizeof(gSetupButtons[0]);
+
+Button gPauseButtons[] =
+{
+    { Vector2{ SCREEN_WIDTH * 0.5f - 140.0f, 270.0f }, "CONTINUE",        PAUSE_BTN_CONTINUE     },
+    { Vector2{ SCREEN_WIDTH * 0.5f - 140.0f, 340.0f }, "SETTING",         PAUSE_BTN_SETTING      },
+    { Vector2{ SCREEN_WIDTH * 0.5f - 140.0f, 410.0f }, "SAVE",            PAUSE_BTN_SAVE         },
+    { Vector2{ SCREEN_WIDTH * 0.5f - 140.0f, 480.0f }, "LOAD",            PAUSE_BTN_LOAD         },
+    { Vector2{ SCREEN_WIDTH * 0.5f - 140.0f, 550.0f }, "EXIT TO MENU",    PAUSE_BTN_EXIT_MENU    },
+    { Vector2{ SCREEN_WIDTH * 0.5f - 140.0f, 620.0f }, "EXIT TO DESKTOP", PAUSE_BTN_EXIT_DESKTOP }
+};
+
+int gPauseButtonCount = sizeof(gPauseButtons) / sizeof(gPauseButtons[0]);
--- CaroGame/src/Control/menu_data.h
+++ CaroGame/src/Control/menu_data.h
@@ -1,15 +1,16 @@
 #pragma once
 #include "raylib.h"
 #include <string>
+using namespace std;
 
-struct NeonButton
+struct Button
 {
     Vector2 position;
-    std::string title;
+    string title;
     int id;
 };
 
-enum ButtonId
+enum MainMenuButtonId
 {
     BTN_PLAY = 0,
     BTN_ABOUT,
@@ -17,13 +18,36 @@ enum ButtonId
     BTN_EXIT
 };
 
+enum SetupButtonId
+{
+    SETUP_BTN_PLAY = 100,
+    SETUP_BTN_BACK
+};
+
+enum PauseButtonId
+{
+    PAUSE_BTN_CONTINUE = 200,
+    PAUSE_BTN_SETTING,
+    PAUSE_BTN_SAVE,
+    PAUSE_BTN_LOAD,
+    PAUSE_BTN_EXIT_MENU,
+    PAUSE_BTN_EXIT_DESKTOP
+};
+
 enum ScreenState
 {
     SCREEN_MAIN_MENU,
     SCREEN_PLAY,
     SCREEN_ABOUT,
-    SCREEN_SETTING
+    SCREEN_SETTING,
+    SCREEN_SETUP
 };
 
-extern NeonButton gButtons[];
-extern int gButtonCount;
+extern Button gMainMenuButtons[];
+extern int gMainMenuButtonCount;
+
+extern Button gSetupButtons[];
+extern int gSetupButtonCount;
+
+extern Button gPauseButtons[];
+extern int gPauseButtonCount;
